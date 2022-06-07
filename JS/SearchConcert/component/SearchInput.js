39918d0611004b8c0cdb0bd1daf4662a1c5f9365
SeungHak5161 <bsh93367@gmail.com>
2022-06-07 21:29:45 +0900

    SearchConcert debounce
    - setTimeout 이용해 Debounce 구현 (delay 1초)

--- JS/SearchConcert/component/SearchInput.js
+++ JS/SearchConcert/component/SearchInput.js
@@ -1,8 +1,15 @@
 export default function SearchInput({ $target, fetchData }) {
   let data = [];
+  let timer;
   $target.addEventListener("keyup", (e) => {
-    if (e.target.value.trim() !== "") {
-      fetchData(e.target.value);
+    if (timer) {
+      clearTimeout(timer);
     }
+    timer = setTimeout(function () {
+      if (e.target.value.trim() !== "") {
+        console.log("검색 시작");
+        fetchData(e.target.value);
+      }
+    }, 1000);
   });
 }
